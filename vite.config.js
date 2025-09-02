bad996c6bf0dc938b2fa3d9e1f4b5b935f999dd4
harshith1459 <150678178+harshith1459@users.noreply.github.com>
2025-09-02 21:10:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      'process.env': env, // Allows using process.env.MY_VAR in the code
+    },
+  });
+};
