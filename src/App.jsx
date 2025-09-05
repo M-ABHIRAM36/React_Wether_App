ae4e16c7c538e9bcaf7c6ae545d002075536ae26
M-ABHIRAM36 <abhicoding36@gmail.com>
2025-09-05 12:01:25 +0530

    Added basic login page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,45 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import WeatherApp from "./WeatherApp";
+import Login from "./login";
+
+function App() {
+  return (
+    <Router>
+      <nav style={{ padding: "10px", background: "#eee" }}>
+        <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<WeatherApp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
 // import './App.css'
 // import Button from '@mui/material/Button';
 
 // import SearchBox from './SearchBox';
 // import InfoBox from './infoBox';
-import WeatherApp from './WeatherApp';
-function App() {
+// import WeatherApp from './WeatherApp';
+// import Login from "./login";
+// function App() {
 
-  return (
-   <div>
-    <WeatherApp/>
-   </div>
-  );
-}
+//   return (
+//    <div>
+//         <div>
+//       <Login />
+//     </div>
+//     <WeatherApp/>
+//    </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
 
 
